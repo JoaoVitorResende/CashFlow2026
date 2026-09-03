818560eceb510bf5b055d44e6acd4e481a790d63
João <joaovitorresende.jr@gmail.com>
2026-09-03 19:05:12 -0300

    chore: first step to create user on api

---
+++ CashFlow/src/CashFlow.Domain/Enuns/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Domain.Enuns
+{
+    public class Roles
+    {
+        public const string ADIM = "admnistrator";
+        public const string TEAM_MEMBER = "teamMember";
+    }
+}

5b153d1d1d6f3218813a5776ef9a268c9b35b663
João <joaovitorresende.jr@gmail.com>
2026-09-09 21:17:45 -0300

    feat: checking current email exists

--- CashFlow/src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -5,28 +5,32 @@ using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
+using FluentValidation.Results;
 
 namespace CashFlow.Application.UseCases.Users.Register
 {
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
         private readonly IUserWriteOnlyRepository _repository;
+        private readonly IUserReadOnlyRepository _repositoryRead;
         private readonly IUnityOfWork _unityOfWork;
         private readonly IPasswordEncripter _passwordEncripter;
         private readonly IMapper _autoMapper;
 
-        public RegisterUserUseCase(IUserWriteOnlyRepository repository, IUnityOfWork unityOfWork, IMapper autoMapper, IPasswordEncripter passwordEncripter)
+        public RegisterUserUseCase(IUserWriteOnlyRepository repository, IUnityOfWork unityOfWork, IMapper autoMapper, IPasswordEncripter passwordEncripter, IUserReadOnlyRepository repositoryRead)
         {
             _repository = repository;
             _unityOfWork = unityOfWork;
             _autoMapper = autoMapper;
             _passwordEncripter = passwordEncripter;
+            _repositoryRead = repositoryRead;
         }
 
         public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUsersJson request)
         {
-            Validate(request);
+            await Validate(request);
             var user = _autoMapper.Map<User>(request);
             user.Password = _passwordEncripter.Encrypt(request.Password);
             await _repository.Add(user);
@@ -38,10 +42,17 @@ namespace CashFlow.Application.UseCases.Users.Register
             };
         }
 
-        private void Validate(RequestRegisterUsersJson request)
+        private async Task Validate(RequestRegisterUsersJson request)
         {
             var validator = new UserValidator();
             var result = validator.Validate(request);
+
+            var emailExists = await _repositoryRead.ExistActiveUserWithEmail(request.Email);
+
+            if (emailExists)
+            {
+                result.Errors.Add(new ValidationFailure(string.Empty,ResourceErrorMessages.EMAILS_EXISTS));
+            }
 
             if (!result.IsValid)
             {
--- CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/UsersRepository.cs
+++ CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/UsersRepository.cs
@@ -1,15 +1,25 @@
 ﻿using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories
 {
-    public class UsersRepository: IUserWriteOnlyRepository
+    internal class UsersRepository: IUserWriteOnlyRepository, IUserReadOnlyRepository
     {
         private readonly CashFlowDbContext _dbcontext;
+
+        public UsersRepository(CashFlowDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
 
         public async Task Add(User user)
         {
             await _dbcontext.Users.AddAsync(user);
+        }
+        public async Task<bool> ExistActiveUserWithEmail(string email)
+        {
+            return await _dbcontext.Users.AnyAsync(user => user.Email.Equals(email));
         }
     }
 }

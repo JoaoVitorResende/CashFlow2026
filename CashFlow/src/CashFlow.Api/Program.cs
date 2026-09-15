9db6b078f3fb6a65f85dd0d075a67ad303c17825
João <joaovitorresende.jr@gmail.com>
2026-09-15 14:44:35 -0300

    feat: configurating authentication.

--- CashFlow/src/CashFlow.Api/Program.cs
+++ CashFlow/src/CashFlow.Api/Program.cs
@@ -3,6 +3,9 @@ using CashFlow.Application;
 using CashFlow.Application.Middleware;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastucture.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+config.TokenValidationParameters = new TokenValidationParameters {
+    ValidateIssuer = false,
+    ValidateAudience = false,
+    ClockSkew = new TimeSpan(0),
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+};
+});
 var app = builder.Build();
 
 app.UseMiddleware<CultureMiddleware>();
@@ -27,6 +44,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
